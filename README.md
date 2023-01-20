f5909f2d8d0ea9e187f043a72b7c981557ddcc53
Nermen <114686822+NermenElefky@users.noreply.github.com>
2023-01-21 00:48:51 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,49 @@
+<!-- Before everything
+
+***** I loaded the starter files and build my own project from scratch
+***** I search for slove for my problems in google and i refere the links belowe.
+**************** I will be happy for any changes to improve my code😊😄
+*********************** Thank you
+ -->
+
+# Landing Page Project
+
+## Table of Contents
+
+* [Overflow](#Overflow)
+* [Built_With](#Built_With)
+* [Adventages](#Adventages)
+* [helper_Links](#helper_Links)
+* [What_I_learned](#What_I_learned)
+* [Author](#Author)
+
+##  Overflow
+This is a responsive landing page project. I used HTML / CSS & JS to create the page contnet.
+
+## Built_With
+Html
+Css
+JS
+FontAwesome
+
+### Adventages
+- I added a button to scroll to top
+- I make the nav disappear after 3 seconds if there is no scroll
+- I make the section in view highlighted with his nav item
+- I make ul items with JS
+
+## helper_Links
+I searches about some functions and a solve for problems I faced  here are the website I searched in :
+
+* [MDN](#https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect)
+* [W3Shcools](#https://www.w3schools.com/jsref/met_element_scrollintoview.asp)
+* [stack Overflow](#https://stackoverflow.com/questions/4620906/how-do-i-know-when-ive-stopped-scrolling)
+
+
+### What_I_learned
+I learned  getBoundingClientRect() function
+Scroll into View
+And I know some more info about JS
+
+## Author
+Created With Nermen
---
+++ js/app.js
@@ -0,0 +1,100 @@
+//Start global vars 
+// selctings all page sections
+let sections = document.querySelectorAll("section");
+
+// selecting the ul which we will add the links to
+let ourList = document.querySelector("ul");
+
+// select the main header (landing page)
+let head = document.querySelector("header.main-heading")
+
+// build the fragment
+let fragment = document.createDocumentFragment();
+// end global vars
+// build the nav
+for (let section of sections){
+    // creating the li element
+    const liElement = document.createElement("li")
+
+    // set the innerHTML (i just added the a element inside the innerhtml AND USED the variable to define the href attr and content of a)
+    liElement.innerHTML = `<a href = "#${section.getAttribute("id")}"class = "menu__link" id ="${section.getAttribute("id")}">${section.getAttribute("data-nav")}</a>`
+
+    // add the li element to the fragment
+    fragment.appendChild(liElement);
+}
+
+// add the fragment to the ul 
+ourList.append(fragment)
+
+// select all ul li 
+let list_elements = document.querySelectorAll("ul li")
+
+// start helper functions
+function remove_section_class_active (){
+    for (let section of sections){
+        section.classList.remove("active")
+    }
+}
+function remove_li_class_active (){
+    for (let element of list_elements){
+        element.classList.remove("active")
+    }
+}
+
+// add the active class to section in viewport
+window.onscroll = function(){
+    for (let section of sections){
+        let q =300;
+        if (window.scrollY >= section.offsetTop - q&& window.scrollY <= section.offsetTop + section.offsetHeight){
+                remove_section_class_active()
+                section.classList.add("active");
+                remove_li_class_active();
+                // add active class to the nav item that refers to the section in viewport
+                document.getElementById(`${section.getAttribute("id")}`).parentElement.classList.add("active");
+
+        }
+    }
+    // make the button (top) appear after some height
+    if (window.scrollY >= 600){
+        btn.style.display ="block"
+    }
+    else {
+        btn.style.display = "none"
+    }
+}
+
+// Scroll to section on link click
+ourList.addEventListener("click",function(e){
+    if (e.target.nodeName === "A"){
+        // prevent the default of anchor tag ===> to control the smooth behavior
+        e.preventDefault();
+
+        //make a loop over the sections to check the href with the id
+        for (const section of sections){
+            // i put # because the href has (#) and the id has no (#)
+            if ("#"+section.getAttribute("id") === e.target.getAttribute("href")){
+               
+                section.scrollIntoView({behavior:"smooth", block : "center"})
+
+            }
+        }
+
+    }
+})
+
+// hide the nav bar if there is no scroll
+var _scroll = null;
+window.addEventListener('scroll', function() {
+    if(_scroll !== null) {
+        clearTimeout(_scroll); 
+        ourList.style.display = "block";    
+    }
+    _scroll = setTimeout(function() {
+         ourList.style.display = "none"
+    }, 3000);
+});
+
+// make the button scroll to top
+btn.onclick = function (){
+    head.scrollIntoView({behavior:"smooth"})
+}
